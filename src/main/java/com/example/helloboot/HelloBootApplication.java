e849603388c645ba465d7855ed8519296418eb30
charles <1061883381@qq.com>
2018-05-17 22:57:10 +0800

    test eureka service registe

--- src/main/java/com/example/helloboot/HelloBootApplication.java
+++ src/main/java/com/example/helloboot/HelloBootApplication.java
@@ -2,7 +2,13 @@ package com.example.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ *  使用@EnableDiscoveryClient 注解用来激活Eureka中的DiscoveryClient实现
+ *  才能实现在HelloController中/hello对服务信息对输出
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloBootApplication {
 
--- src/main/java/com/example/helloboot/springBoot/controller/HelloController.java
+++ src/main/java/com/example/helloboot/springBoot/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.helloboot.springBoot.controller;
 
 import com.example.helloboot.springBoot.pojo.AuthorSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     /**
      * 可以通过@Value注解直接将properties中的配置读取出来
@@ -25,8 +32,18 @@ public class HelloController {
     @Autowired
     private AuthorSettings authorSettings;
 
-    @RequestMapping("/hello")
+    /**
+     * 注入该对象，为了能够在日志中打印服务对相关内容，并且我们需要在applcation.properties文件中
+     * 通过配置spring.application.name属性来为服务命名
+     * 通过配置eurake.client.serviceUrl.defaultZone属性来指定服务注册中心的地址
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
+        ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+        LOGGER.info("/hello,host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
         return "hello,this is a springBoot demo!!!author:" + author + ", name:" + name;
     }
 
